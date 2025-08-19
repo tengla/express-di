3d463463a24265e494646bff876812a812aeebdd
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2025-08-19 21:08:56 +0200

    refactor: remove log statement

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 import express from "express";
 import AppointmentsRoute from "./routes/appointments";
 
-const app = express();
+const app = express()
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  // Middleware logic here
   const authHeader = req.headers['authorization'];
   if(!authHeader) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
   if (authHeader) {
-    // Do something with the authHeader
-    console.log(`Authorization header: ${authHeader}`);
     req.user = { id: authHeader.slice('Bearer '.length) };
   }
   next();
 });
+
 app.use('/appointments', AppointmentsRoute);
 
 export default app;
